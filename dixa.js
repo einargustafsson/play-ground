26104016a3f65bcff2169cd85793091702937477
Einar Gustafsson <einargustafsson@users.noreply.github.com>
2023-09-06 17:46:08 +0000

    adding chat

--- dixa.js
+++ dixa.js
@@ -11,9 +11,35 @@ submitButton.addEventListener("click", function() {
 
     // Iterate through the FormData object
     for (const [name, value] of formData) {
-        combinedValues += `${name}:\n${value}\n\n`;
+        if(`${name}` != "message" && `${name}` != "subject"  && `${name}` != "email"  && `${name}` != "name"){
+            combinedValues += `${name}: ${value}\n\n`;
+        }
     }
 
     // Set the combined values in the textarea
     textarea.value = combinedValues;
    });
+
+
+(function (w, d, s) {
+  if (w._dixa_) return;
+  w._dixa_ = {
+    invoke: function (m, a) {
+      w._dixa_.pendingCalls = w._dixa_.pendingCalls || [];
+      w._dixa_.pendingCalls.push([m, a]);
+    },
+    addListener: function (event, listener) {
+      w._dixa_.pendingAddListenerCalls = w._dixa_.pendingAddListenerCalls || [];
+      w._dixa_.pendingAddListenerCalls.push([event, listener]);
+    },
+  };
+  s = d.createElement('script');
+  s.type = 'text/javascript';
+  s.setAttribute('charset', 'utf-8');
+  s.async = true;
+  s.src = 'https://messenger.dixa.io/bootstrap.js';
+  var before = d.getElementsByTagName('script')[0];
+  before.parentNode.insertBefore(s, before);
+})(window, document);
+_dixa_.invoke('init', { messengerToken: '36ec56fce8394e9dbc347d025a621bc5' });
+
